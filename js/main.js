ba65e8b31c220f7dc4572a2e8c58b5dd787d0d69
Nestor Emmanuel Gonzalez <98758713+el-guey@users.noreply.github.com>
2023-07-03 15:25:23 -0300

    Web navigator Mozilla Firefox
    Archivos del front

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+// navbar-light bg-light">
+// <div class="container">
+// <a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+
+// </div>
+// </nav>`
+
+document.getElementById("header").innerHTML=`
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<div class="container">
+    <a class="navbar-brand" href="../templates/libros.html" alt:"Inicio">
+        <i class="fa-solid fa-book-open-reader"></i> Almacen de Libros
+    </a>
+</div>
+</nav>`
+
+document.getElementById("footer").innerHTML=`
+<p style="text-align=center">Almacen de Libros &copy; Todos los derechos reservados</p>`
---
+++ js/producto_editar.js
@@ -0,0 +1,66 @@
+console.log(location.search) // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)
+console.log(id)
+const { createApp } = Vue
+createApp({
+data() {
+return {
+id:0,
+titulo:"",
+autor:"",
+imagen:"",
+stock:0,
+precio:0,
+//url:'http://promero.pythonanywhere.com/productos/'+id,
+//url:'http://localhost:5000/productos/'+id,
+url:'http://fido9397.pythonanywhere.com/productos/'+id,
+}
+},
+methods: {
+fetchData(url) {
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+console.log(data)
+this.id=data.id
+this.titulo = data.titulo;
+this.autor=data.autor
+this.imagen=data.imagen
+this.stock=data.stock
+this.precio=data.precio
+})
+.catch(err => {
+console.error(err);
+this.error=true
+})
+},
+modificar() {
+let producto = {
+titulo:this.titulo,
+autor:this.autor,
+precio: this.precio,
+stock: this.stock,
+imagen:this.imagen
+}
+var options = {
+body: JSON.stringify(producto),
+method: 'PUT',
+headers: { 'Content-Type': 'application/json' },
+redirect: 'follow'
+}
+fetch(this.url, options)
+.then(function () {
+alert("Registro modificado")
+window.location.href = "../templates/libros.html";
+})
+.catch(err => {
+console.error(err);
+alert("Error al Modificar")
+})
+}
+},
+created() {
+this.fetchData(this.url)
+},
+}).mount('#app')
